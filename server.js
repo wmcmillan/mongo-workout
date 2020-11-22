9a0dda3f2875a2920b27411ab80337a9afd0367f
Wes <wmcmillan1012@gmail.com>
2020-11-22 12:13:29 -0800

    attempting to fix db connect

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/activity", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.use(require("./routes/api.js"));
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
