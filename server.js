881f97c8721756c6c3467e45960d6504347ad28a
Wes <wmcmillan1012@gmail.com>
2020-11-22 12:15:45 -0800

    changed syntax

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/activity", {
   useFindAndModify: false
 });
 
-app.use(require("./routes/api.js"));
+app.use(require("./controller/fitness-controller.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
