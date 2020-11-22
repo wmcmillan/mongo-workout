bff308398f2c2103307df3d9d074de198721d6f0
Wes <wmcmillan1012@gmail.com>
2020-11-22 12:12:21 -0800

    reverted

--- server.js
+++ server.js
@@ -4,16 +4,6 @@ const PORT = process.env.PORT || 3000
 
 const app = express();
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/fitness',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
